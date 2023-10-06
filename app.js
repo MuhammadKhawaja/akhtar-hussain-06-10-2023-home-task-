804f201ba46c8cd7971c49468303537d63ee7f18
Muhammad Khawaja <mdkhawaja2427@gmail.com>
2023-10-06 16:54:01 -0700

    files addd

---
+++ app.js
@@ -0,0 +1,69 @@
+// Q1
+// function greetUser(firstName, lastName) {
+//     return "hello," + firstName + " " + lastName + "!";
+//   }
+//   Q2
+// function addNumbers(num1, num2) {
+//     return num1 + num2;
+}
+// Q3  
+// function calculate(num1, num2, operator) {
+//     let result;
+//     switch (operator) {
+//       case "+":
+//         result = num1 + num2;
+//         break;
+//       case "-":
+//         result = num1 - num2;
+//         break;
+//       case "*":
+//         result = num1 * num2;
+//         break;
+//       case "/":
+//         result = num1 / num2;
+//         break;
+//       default:
+//         result = "wrong number";
+//     }
+//     return result;
+//   }
+  
+//   Q4
+
+// function square(num) {
+//     return num * num;
+//   }
+
+//   Q5
+
+// function factorial(num) {
+//     if (num < 0)
+//       return -1;
+//     else if (num === 0)
+//       return 1;
+//     else {
+//       return num * factorial(num - 1);
+//     }
+//   }
+
+//   Q6
+
+// function displayCount(start, end) {
+//     for (let i = start; i <= end; i++) {
+//       console.log(i);
+//     }
+//   }
+  
+//   Q7
+
+// function calculateRectangleArea(width, height) {
+//     return width * height;
+//   }
+
+//   Q8
+
+// function capitalizeFirstLetter(str) {
+//     const words = str.split(" ");
+//     const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+//     return capitalizedWords.join(" ");
+//   }
